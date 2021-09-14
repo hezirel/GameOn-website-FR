05a51e6c1258edd45f9baca6966cd670cb10302b
hezirel <hezirel@gmail.com>
2021-09-14 12:20:48 +0200

    added js regex email validation funtion

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,6 +12,7 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const closeModal = document.querySelectorAll(".close");
+const emailForm = document.querySelectorAll(".email")
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -22,8 +23,21 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// hide modal form on .close .btn press (!(launchModal))
 function hideModal() {
+  //modalbg.removeAttribute("style");
   modalbg.style.display = "none";
 }
 
+emailForm.addEventListener("change", valEmail());
 
+function valEmail(mail)
+{
+  if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(myForm.emailAddr.value)) {
+    return (true)
+  }
+  document.querySelector("emailHint").setAttribute("display = block")
+  return (false)
+}
+
+emailForm.addEventListener('change', valEmail(e))
