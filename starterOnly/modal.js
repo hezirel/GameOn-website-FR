a40db36bf4c491138c791a66b5d9163d3565c84f
hezirel <hezirel@gmail.com>
2021-09-18 18:45:16 +0200

    commit before creating form submit validate function

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -8,14 +8,16 @@ function editNav() {
 }
 
 // DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
-const closeModal = document.querySelectorAll(".close");
+const modalbg = document.querySelector(".bground")
+const modalBtn = document.querySelectorAll(".modal-btn")
+const submitBtn = document.querySelector(".btn-submit")
+const formData = document.querySelectorAll(".formData")
+const closeModal = document.querySelectorAll(".close")
 const firstInput = document.getElementById("first")
 const lastInput = document.getElementById("last")
 const emailInput = document.getElementById("email")
 const birthInput = document.getElementById("birthdate")
+let minAge = 18
 const quantityInput = document.getElementById("quantity")
 //const radioInput = document.getElementsByName('location')
 //const radioField = document.getElementById("locationsRadio")
@@ -27,11 +29,18 @@ const checkboxInput = document.getElementById("checkbox1")
 modalBtn.forEach((btn) => btn.addEventListener("click", toggleModal));
 // close event
 closeModal.forEach((btn) => btn.addEventListener("click", toggleModal));
+//submitModal.forEach((btn) => btn.addEventListener("click", toggleModal));
 
 // launch modal form
 function toggleModal() {
   // Explain conditon bolean
   modalbg.style.display = modalbg.style.display === 'block' ? '' : 'block';
+}
+
+
+//submit conditions check before submit
+function validate() {
+  return true;
 }
 
 //hide modal form on .close .btn press (!(launchModal))
@@ -43,36 +52,39 @@ function toggleModal() {
 firstInput.addEventListener("focusout", function (e) {
   var re = /^[A-Za-z]{2,20}$/
   //if ((firstInput.value.length < 2 || firstInput.value.length > 20) || (!(firstInput.value.trim().match(re)))) {
-  if (!(firstInput.value.trim().match(re))) {
+  if (!(e.target.value.trim().match(re))) {
     document.getElementById("nameHint").style.display = "inline"
-    firstInput.parentElement.setAttribute('data-error-visible', 'true');
+    e.target.parentElement.setAttribute('data-error-visible', 'true');
     return false;
   }
-  firstInput.parentElement.setAttribute('data-error-visible', 'false');
+  e.target.parentElement.setAttribute('data-error-visible', 'false');
   document.getElementById("nameHint").style.display = "none"
+  return true;
 });
 
 lastInput.addEventListener("focusout", function (e) {
   var re = /^[A-Za-z]{2,20}$/
-  if (!(lastInput.value.trim().match(re))) {
+  if (!(e.target.value.trim().match(re))) {
     document.getElementById("lastHint").style.display = "inline"
-    lastInput.parentElement.setAttribute('data-error-visible', 'true');
+    e.target.parentElement.setAttribute('data-error-visible', 'true');
     return false;
   }
-  lastInput.parentElement.setAttribute('data-error-visible', 'false');
+  e.target.parentElement.setAttribute('data-error-visible', 'false');
   document.getElementById("lastHint").style.display = "none"
+  return true;
 });
 
 //Talk about others events possible
 emailInput.addEventListener('focusout', function (e) {
   const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!(emailInput.value.trim().match(re))) {
-    emailInput.parentElement.setAttribute('data-error-visible', 'true');
+  if (!(e.target.value.trim().match(re))) {
+    e.target.parentElement.setAttribute('data-error-visible', 'true');
     document.getElementById("emailHint").style.display = "block"
-    return (false);
+    return false;
   }
-  emailInput.parentElement.setAttribute('data-error-visible', 'false');
+  e.target.parentElement.setAttribute('data-error-visible', 'false');
   document.getElementById("emailHint").style.display = "none"
+  return true;
 })
 
 function birthValid(e) {
@@ -80,25 +92,28 @@ function birthValid(e) {
   //  if(e.match)
   return true;
 }
+
 birthInput.addEventListener("focusout", function (e) {
-  if (!(birthValid(birthInput.value))) {
+  if (!(birthValid(e.target.value))) {
     document.getElementById("birthHint").style.display = "inline"
-    birthInput.parentElement.setAttribute('data-error-visible', 'true');
+    e.target.parentElement.setAttribute('data-error-visible', 'true');
     return false;
   }
-  birthInput.parentElement.setAttribute('data-error-visible', 'false');
+  e.target.parentElement.setAttribute('data-error-visible', 'false');
   document.getElementById("birthHint").style.display = "none"
+  return false;
 });
 
 quantityInput.addEventListener("focusout", function (e) {
   const re = /^([0-9](?:[0-9])?)$/;
-  if (!(quantityInput.value.match(re))) {
+  if (!(e.target.value.match(re))) {
     document.getElementById("quantityHint").style.display = "inline"
-    quantityInput.parentElement.setAttribute('data-error-visible', 'true');
+    e.target.parentElement.setAttribute('data-error-visible', 'true');
     return false;
   }
-  quantityInput.parentElement.setAttribute('data-error-visible', 'false');
+  e.target.parentElement.setAttribute('data-error-visible', 'false');
   document.getElementById("quantityHint").style.display = "none"
+  return true;
 });
 
 //Change event listener for something else on which location verif is triggered
@@ -114,9 +129,10 @@ quantityInput.addEventListener("focusout", function (e) {
   }
 });
  */
+
 checkboxInput.addEventListener('change', function (e) {
-  if (!(checkboxInput.checked)) {
-    checkboxInput.parentElement.setAttribute('data-error-visible', 'true');
+  if (!(e.target.checked)) {
+    e.target.parentElement.setAttribute('data-error-visible', 'true');
     document.getElementById("checkboxHint").innerText = "Vous devez accepter les termes et conditions d'utilisation";
     return false;
   }
